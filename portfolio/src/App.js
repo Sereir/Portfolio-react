308e1a5f3f0f32d255a01eefe3c282714323e67d
Nacim <nacim.sereir@ynov.com>
2023-07-11 16:54:14 +0200

    premier push

---
+++ portfolio/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import pages
+import About from './pages/about/about';
+import Error from './pages/error/error';
+import Experience from './pages/experience/experience';
+import Home from './pages/home/home';
+import Contact from './pages/contact/contact';
+// import components
+import Navbar from './components/navbar/navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='projects' element={<Experience/>} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/navbar/navbar.js
@@ -0,0 +1,68 @@
+import { Link } from 'react-router-dom'
+import logo from '../../images/logo.svg'
+import { useEffect, useState } from 'react';
+
+export default function Navbar() {
+  const [isNavAffix, setIsNavAffix] = useState(false);
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsNavAffix(true);
+      } else {
+        setIsNavAffix(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const closeMenu = () => {
+    setIsMenuActive(false);
+  }
+
+  const handleMenuClick = () => {
+    setIsMenuActive(!isMenuActive);
+  };
+
+  return (
+    <nav className={`nav ${isNavAffix ? 'affix' : ''}`}>
+      <div className='container'>
+        <div className='logo'>
+        <Link to='/'>
+          <img src={logo} alt='cocktail db logo' className='mainlogo'/>
+        </Link>
+      
+        </div>
+      
+        <div id='mainListDiv' className={`main_list ${isMenuActive ? 'show_list' : ''}`}>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/' onClick={closeMenu}>home</Link>
+          </li>
+          <li>
+            <Link to='/projects' onClick={closeMenu}>projects</Link>
+          </li>
+          <li>
+            <Link to='/contact' onClick={closeMenu}>contact</Link>
+          </li>
+          <li>
+            <Link to='/about' onClick={closeMenu}>about</Link>
+          </li>
+        </ul>
+        </div>
+        <span className={`navTrigger ${isMenuActive ? 'active' : ''}`} onClick={handleMenuClick}>
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
+        </div>
+    </nav>
+  )
+}
+
