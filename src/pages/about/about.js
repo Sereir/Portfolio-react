a10ef60b9c3bb139a92935cfbd92e08f520acfd3
Nacim <nacim.sereir@ynov.com>
2023-08-03 15:18:06 +0200

    First commit

---
+++ src/pages/about/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Particles from "../../components/particles/particles";
+import Technologies from "../../components/technologies/technologies";
+
+function About() {
+  return (
+    <div className="test">
+      <Particles />
+      <Technologies />
+    </div>
+  );
+}
+
+export default About;
